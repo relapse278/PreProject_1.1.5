2ddef589f3abbd95933d97ee6afbaec7bb9f6616
Руслан Шевченко <r.schewtschenko@mail.ru>
2023-08-17 00:37:54 +0700

    Удалены импорты без звезд.

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,7 +2,10 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import jm.task.core.jdbc.util.Util;
